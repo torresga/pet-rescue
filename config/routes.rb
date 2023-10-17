ac09f000dfe2b70e2462b37e716af9e6473d1bf5
Erin Claudio <eclaudio@gmail.com>
2023-10-17 14:34:39 -0400

    Added new partials and Table to DB

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,10 @@ Rails.application.routes.draw do
     resource :organization_profile, only: %i[edit update]
 
     resources :home, only: [:index]
-    resources :pets
+    resources :pets do
+      post 'update_images', on: :member, to: 'pets#update_images'
+      resources :tasks, only: [:create, :edit, :update, :destroy]
+    end
     resources :dashboard
     resources :adoption_application_reviews, only: [:index, :edit, :update]
     resources :foster_application_reviews, only: [:index]
